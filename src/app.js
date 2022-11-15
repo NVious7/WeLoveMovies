0c7c8286d2560ad144725650355a97bf31da86c8
nvious7 <nghia7van@gmail.com>
2022-11-15 13:33:38 -0500

    Revision 1

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 if (process.env.USER) require("dotenv").config();
 const express = require("express");
-const app = express();
 const cors = require("cors");
+const app = express();
 const moviesRouter = require("./movies/movies.router");
 const reviewsRouter = require("./reviews/reviews.router");
 const theatersRouter = require("./theaters/theaters.router");
